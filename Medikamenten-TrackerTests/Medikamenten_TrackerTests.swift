b34b95604431d16318149069f124b24c1541ba9b
Andreas Liedke <al@liedke.info>
2026-04-02 15:07:21 +0200

    Initial Commit

---
+++ Medikamenten-TrackerTests/Medikamenten_TrackerTests.swift
@@ -0,0 +1,18 @@
+//
+//  Medikamenten_TrackerTests.swift
+//  Medikamenten-TrackerTests
+//
+//  Created by Andreas Liedke on 02.04.26.
+//
+
+import Testing
+
+struct Medikamenten_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
